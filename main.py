af7a219159f5c4ccedc25df9cc4a76e9197d247c
GeorgeGozali <giorgi.gozalishvili.1@iliauni.edu.ge>
2023-02-07 14:30:13 +0400

    refactored

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from map import map_dict
 
 
 class Map:
+    current_location = "West Of House"
+
+    def __init__(self):
+        if self.__dict__.get("place") is None:
+            self.move(**map_dict.get(self.current_location))
+
     def move(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+        self.current_location = self.__dict__.get("place")
 
+    def look(self):
+        print(self.__dict__.get("place"))
+        print(self.__dict__.get("description"), "\n")
 
 
 # class BASE(Map):
@@ -14,25 +24,26 @@ class Map:
 
 class Player(Map):
     _health = 100
-    current_location = map_dict.get("West Of House")
 
     @property
     def health(self):
         return self._health
 
     @health.setter
-    def health(self, new_value):
+    def health(self, new_value: int) -> None:
         self._health = new_value
 
-    def move(self, direction):
-        new_direction = self.current_location['exits'][direction]
-        if map_dict.get(new_direction):
-            super().move(**map_dict.get(new_direction))
-            self.current_location = map_dict.get(new_direction)
-        elif new_direction is None:
-            print("You can't go that way.")
+    def moves(self, direction):
+        new_direction = self.__dict__.get("exits").get(direction)
+        if new_direction:
+            try:
+                super().move(**map_dict.get(new_direction))
+                self.current_location = map_dict.get(new_direction)
+                self.look()
+            except TypeError:
+                print(new_direction, "\n")
         else:
-            print(new_direction)
+            print("You can't go that way.\n")
 
 
 player = Player()
@@ -43,9 +54,10 @@ while True:
         user_input = input("Are you sure you want to quit? ")
         while user_input.lower() not in ("y", "yes", "n", "no"):
             user_input = input("Please answer yes or no.>")
+        if user_input.lower() in ("y", "yes"):
+            break
     elif user_input == "look":
-        print(player.current_location.get("place"))
-        print(player.current_location.get("description"), "\n")
+        player.look()
     elif user_input in ("n", "s", "e", "w", "ne", "nw", "se", "sw"):
-        player.move(user_input)
-        print(player.current_location['place'], "\n")
+        player.moves(user_input)
+        # print(dir(player))
