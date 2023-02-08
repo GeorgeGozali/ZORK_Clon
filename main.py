f39188f4b70a21a3a1b1f37ef6c18b4da2ebd524
GeorgeGozali <giorgi.gozalishvili.1@iliauni.edu.ge>
2023-02-08 14:10:57 +0400

    added TODOs

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-from map import map_dict
+import json
+
+DEFAULT_HEALTH = 100
+START_LOCATION = "West Of House"
 
 
-class Map:
-    current_location = "West Of House"
+class Place:
+    def __init__(self, place, description, exits):
+        self.place = place
+        self.description = description
+        self.exits = exits
 
+    def __str__(self):
+        return self.place
+
+
+class Map(Place):
     def __init__(self):
-        if self.__dict__.get("place") is None:
-            self.move(**map_dict.get(self.current_location))
+        self.map_dict = {}
+        self.load_map()
+        # self.place = Place(**self.load_map())
 
-    def move(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-        self.current_location = self.__dict__.get("place")
+    def load_map(self):
+        with open("map.json", "r") as f:
+            self.map_dict = json.load(f)
 
-    def look(self):
-        print(self.__dict__.get("place"))
-        print(self.__dict__.get("description"), "\n")
-
-
-# class BASE(Map):
-#     pass
+    def get(self, key):
+        return self.map_dict.get(key)
 
 
-class Player(Map):
-    _health = 100
+class Player():
+    _health = DEFAULT_HEALTH
+    map = Map()
 
     @property
     def health(self):
@@ -33,31 +40,44 @@ class Player(Map):
     def health(self, new_value: int) -> None:
         self._health = new_value
 
-    def moves(self, direction):
-        new_direction = self.__dict__.get("exits").get(direction)
-        if new_direction:
-            try:
-                super().move(**map_dict.get(new_direction))
-                self.current_location = map_dict.get(new_direction)
-                self.look()
-            except TypeError:
-                print(new_direction, "\n")
-        else:
-            print("You can't go that way.\n")
+    def move(self, direction):
+        print(map.__dict__)
+        # new_direction = map.
+        # if new_direction:
+        #     self.current_location = map_dict.get(new_direction)
+        #     if self.current_location:
+        #         self.update(new_location=new_direction)
+        #         self.look()
+        #     else:
+        #         print(new_direction, "\n")
+        # else:
+        #     print("You can't go that way.\n")
+
+    # TODO change so it no longer uses class attributes but map/room objects
+    def look(self):
+        print(self.__dict__.get("place"))
+        print(self.__dict__.get("description"), "\n")
 
 
 player = Player()
 
-while True:
-    user_input = input(">")
-    if user_input == "q":
-        user_input = input("Are you sure you want to quit? ")
-        while user_input.lower() not in ("y", "yes", "n", "no"):
-            user_input = input("Please answer yes or no.>")
-        if user_input.lower() in ("y", "yes"):
-            break
-    elif user_input == "look":
-        player.look()
-    elif user_input in ("n", "s", "e", "w", "ne", "nw", "se", "sw"):
-        player.moves(user_input)
-        # print(dir(player))
+
+def run():
+    while True:
+        user_input = input(">")
+        if user_input == "q":
+            user_input = input("Are you sure you want to quit? ")
+            while user_input.lower() not in ("y", "yes", "n", "no"):
+                user_input = input("Please answer yes or no.>")
+            if user_input.lower() in ("y", "yes"):
+                break
+        elif user_input == "look":
+            player.look()
+        elif user_input in ("n", "s", "e", "w", "ne", "nw", "se", "sw"):
+            player.move(user_input)
+
+
+if __name__ == '__main__':
+    player = Player()
+    # player.map = Map()
+    run()
